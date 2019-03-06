391e2d3183f4fa581596c4bb6ccad0bd951e9386
Sean Kelley <sean@305spin.com>
2019-03-06 12:57:51 -0700

    adding express and socket.io to allow for messaging

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('hello from our node script!');
+/*global console, require */
+/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+    socket.on('message', function(msg){
+        io.emit('message', msg);
+    });
+});
+
+app.use(express.static('client'));
+
+server.listen(8080, function() {
+  console.log('Chat server running');
+});
