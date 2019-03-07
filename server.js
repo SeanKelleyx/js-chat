81c8f5502503d51ca2bf315d4f7e3e33405fd9fb
Sean Kelley <sean@305spin.com>
2019-03-07 15:32:07 -0700

    adding ability for weather bot to lookup location info to get weather based on request

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 /*eslint no-console: ["error", { allow: ["warn", "error"] }] */
 var express = require('express');
 var app = express();
-
+var request = require('request');
 var http = require('http');
 var server = http.Server(app);
 var io = require('socket.io')(server);
@@ -22,6 +22,9 @@ function sendMessage(msg){
     messages.push(msg);
     io.emit('message', msg);
 }
+function sendPrompt(msg){
+    io.emit('message', msg);
+}
 
 // respond to valid questions
 function respond(msg){
@@ -30,16 +33,17 @@ function respond(msg){
             respondWithTime();
         }
         if(askingWeather(msg)){
-            respondWithWeather(msg);
-            /*
-            // if has zip?
-            if(messageHasZip(msg)){
+            //respondWithWeather(msg);
+            
+            // if has location?
+            if(messageHasLocation(msg)){
                 // get weather
                 respondWithWeather(msg);
-                
             }else{
                 // prompt for zip
-            }*/
+                sendPrompt(buildBotMessage('please include a city name'));
+                sendPrompt(buildBotMessage('like what is the weather in San Diego?'));
+            }
         }
     }
 }
@@ -64,36 +68,68 @@ function askingWeather(msg){
     return msg.text.toLowerCase().match(/weather/i);
 }
 
-// message has 5 digit zip code
-function messageHasZip(msg){
-    return msg.text.match(/\bd{5}\b/g);
+// message has a location
+function messageHasLocation(msg){
+    return msg.text.toLowerCase().match(/weather\Win\W(.+)\?/g);
 }
 
 function respondWithWeather(msg){
     //let zip = msg.text.match(/\bd{5}\b/g)[0];
-    let zip = '';
-    getWeather(zip);
+    let cityRegex = /weather\Win\W(.+)\?/g;
+    let matches = cityRegex.exec(msg.text.toLowerCase());
+    let city = matches[1];
+    getWoeidInfo(city, function(city){
+        //error callback
+        sendMessage(buildBotMessage('There was an error getting location info for ' + city + '.'));
+    }, function(locationData){
+        //success callback
+        getWeather(locationData, function(city){
+            //error callback
+            sendMessage(buildBotMessage('There was an error getting the weather for ' + locationData.city + '.'));
+        }, function(data){
+            //success callback
+            sendMessage(buildBotMessage('In ' + data.title + ': ' + data.consolidated_weather[0].weather_state_name));
+        });
+    });
 }
 
 //get weather info
-function getWeather(zip){
-    var request = require('request');
-    request.get("https://www.metaweather.com/api/location/4118/", function(error, response){
+function getWeather(locationData, errorCallback, successCallback){
+    request.get("https://www.metaweather.com/api/location/" + locationData.woeid + "/", function(error, response){
         if(!error && response.statusCode == 200){
             var data = JSON.parse(response.body);
-            sendMessage(buildBotMessage('The weather is: ' + data.consolidated_weather[0].weather_state_name));
+            successCallback(data);
         }else{
             // send weather error
-            sendMessage(buildBotMessage('There was an error getting the weather.'));
+            errorCallback(locationData);
         }
-    })
+    });
+}
+
+//get where on earth id from city name
+function getWoeidInfo(city, errorCallback, successCallback){
+    request.get("https://www.metaweather.com/api/location/search/?query=" + city, function(error, response){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(response.body);
+            if(data.length < 1){
+                errorCallback(city);
+            }else{
+                successCallback({
+                    city: data[0].title,
+                    woeid: data[0].woeid,
+                });
+            }
+        }else{
+            errorCallback(city);
+        }
+    });
 }
 
 function buildBotMessage(text){
     return {
         text: text,
         timestamp: '' + new Date(),
-        initials: 'BT',
+        initials: 'BOT',
     };
 }
 app.use(express.static('client'));
